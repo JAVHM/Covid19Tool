7a08a8c7706e299e392a1d109d9d581df9a17d3c
SebasLV12 <sebasr_12@hotmail.com>
2022-06-20 23:27:14 -0500

    ReadData WO get

--- app/src/main/java/pe/edu/ulima/covid19tool/MainActivity.kt
+++ app/src/main/java/pe/edu/ulima/covid19tool/MainActivity.kt
@@ -3,6 +3,7 @@ package pe.edu.ulima.covid19tool
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
+import androidx.room.Database
 import androidx.room.Room
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -15,23 +16,28 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-       // readCSV()
+        readCSV()
         //DATABASE
-        val room=Room.databaseBuilder(this,CasosDB::class.java,"casos").build()
+        val room=Room.databaseBuilder(this,CasosDB::class.java,"casosBD").build()
 
         lifecycleScope.launch(Dispatchers.IO){
-
-            room.casoPositivoDAO().insert(PositivosObjTemp(6,"LIMA",121212))
-            room.casoPositivoDAO().insert(PositivosObjTemp(7,"AQP",121212))
-            room.casoPositivoDAO().insert(PositivosObjTemp(8,"CHIM",121212))
-
+            //room.casoPositivoDAO().insert(PositivosObjTemp(1,"lima",1231))
+           // room.casoPositivoDAO().insert(PositivosObjTemp(2,"lima",1231))
+            //room.casoPositivoDAO().insert(PositivosObjTemp(3,"lima",1231))
+//            listTemp.forEach{
+//                    item->
+//                room.casoPositivoDAO().insert(PositivosObjTemp(item.ID,item.DEPARTAMENTO,item.FECHA))
+//                println("item"+item.ID+""+item.FECHA+""+item.DEPARTAMENTO)
+//            }
             val casos = room.casoPositivoDAO().obtenerCasos()
             for (item in casos){
                 println("${item.ID},${item.DEPARTAMENTO},${item.FECHA}")
             }
             for (item in casos){
-                room.casoPositivoDAO().delete(item.ID)
+                println("prueba")
+                println("${item.ID},${item.DEPARTAMENTO},${item.FECHA}")
             }
+
         }
 
     }
@@ -46,14 +52,23 @@ class MainActivity : AppCompatActivity() {
         var line : String?
         var displayData : String = ""
 
-        val count: Int = 1
+        var count: Int = 4
+        var countRow: Int=0
         while (reader.readLine().also { line = it } != null){
             val row : List<String> = line!!.split(";")
 
-            val ptemp = PositivosObjTemp(1, row[1], row[7].toInt())
+            if(countRow!=0){
+                val ptemp = PositivosObjTemp(count, row[1], row[7].toInt())
+                count++;
 
-            listTemp.add(ptemp)
-            println("Se ha agregado:" + ptemp.ID + " " + ptemp.DEPARTAMENTO + " " + ptemp.FECHA)
+                listTemp.add(ptemp)
+                println("Se ha agregado:" + ptemp.ID + " " + ptemp.DEPARTAMENTO + " " + ptemp.FECHA)
+            }
+            countRow++
         }
+//        listTemp.forEach{
+//            item->
+//            println("item"+item.ID+""+item.FECHA+""+item.DEPARTAMENTO)
+//        }
     }
 }
