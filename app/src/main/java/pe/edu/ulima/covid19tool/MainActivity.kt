fbb579b4d53f93fe9d29754c0d315e3f525a2cf3
JAVHM <55992367+JAVHM@users.noreply.github.com>
2022-06-20 23:53:08 -0500

    Data base 3

--- app/src/main/java/pe/edu/ulima/covid19tool/MainActivity.kt
+++ app/src/main/java/pe/edu/ulima/covid19tool/MainActivity.kt
@@ -18,7 +18,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //readCSV()a
+        //readCSV()aa
         //DATABASE
         val room=Room.databaseBuilder(this,CasosDB::class.java,"casosBD").build()
 
